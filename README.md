790e2c09e1f7250855689a305fb6d50a4e181fa3
Hex <hex@codeigniter.org.cn>
2021-06-25 17:15:10 +0800

    init

--- README.md
+++ README.md
@@ -1,1 +1,75 @@
 # vite-plugin-virtual-html-template
+
+HTML template for vite app, support flexible virtual URL.
+
+The idea and part of the code for this plugin comes from vite-plugin-html-template.
+
+## Motivation
+
+- Vite need html for entry file, which means we must have
+  - projectRoot/index.html for SPA
+  - projectRoot/app1.html, projectRoot/app2.html for MPA
+  - projectRoot/src/pages/*/index.html for MPA
+- Why not we use html template for all entry html
+- Also we should support lodash.template syntax for the html content, like setting `<title></title>`.
+
+## Usage
+
+```sh
+npm i --save-dev vite-plugin-virtual-html-template
+```
+
+```js
+// vite.config.ts
+import virtualHtmlTemplate from 'vite-plugin-virtual-html-template'
+
+// @see https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    // ...other plugins
+    virtualHtmlTemplate(/* options */)
+  ]
+})
+```
+
+## Options
+
+```js
+// you can custom template path(default is public/index.html) and page title
+{
+  // define pages, if SPA uses this plugin, the index page must be set
+  pages: {
+    index: {
+      template: 'public/index.html',
+      title: 'Home Page',
+      entry: 'src/main.js'
+    },
+    app: {
+      template: 'public/index.html',
+      title: 'App Page',
+      entry: 'src/app/main.js'
+    },
+    'sub/other': {
+      template: 'public/index.html',
+      title: 'Sub Page',
+      entry: 'src/sub/other/main.js'
+    }
+  },
+  // expose to template
+  data: {
+    title: 'Home Page',
+  }
+}
+```
+
+After starting the dev server, browse:
+
+* http://127.0.0.1/index.html : Use `public/index.html` as the template and `src/main.js` as the entry.
+* http://127.0.0.1/app.html : Use `public/index.html` as the template and `src/app/main.js` as the entry.
+* http://127.0.0.1/sub/other.html : Use `public/index.html` as the template and `src/sub/other/main.js` as the entry.
+
+The URL structure after the project is constructed is the same as that during development:
+
+* http://domain.com/index.html
+* http://domain.com/app.html
+* http://domain.com/sub/other.html
---
+++ index.js
@@ -0,0 +1,115 @@
+import path from 'path';
+import { promises as fs } from 'fs';
+import { template } from 'lodash';
+
+const resolve = (p) => path.resolve(process.cwd(), p);
+const relative = (p) => path.relative(process.cwd(), p);
+
+const readHtmlTemplate = async (templatePath) => {
+  return await fs.readFile(templatePath, { encoding: 'utf8' })
+};
+
+const getHtmlContent = async (payload) => {
+  const { templatePath, pageTitle, pageEntry, data } = payload
+  let content = ''
+
+  try {
+    content = await readHtmlTemplate(templatePath);
+  }
+  catch (e) {
+    console.error(e)
+  }
+  content = content.replace(
+    '</body>',
+    `  <script type="module" src="/${pageEntry}"></script>\n</body>`
+  )
+  const compiled = template(content)
+  const context = {
+    title: pageTitle,
+    ...data
+  }
+  const html = compiled({
+    ...context
+  })
+  return html
+}
+
+const virtualHtmlTemplatePlugin = (options) => {
+  return {
+    name: 'vite-plugin-virtual-html-template',
+
+    configureServer(server) {
+      server.middlewares.use(async (req, res, next) => {
+        const url = req._parsedUrl.pathname;
+
+        if (path.extname(url) !== '.html' && url !== '/') {
+          return next()
+        }
+
+        let pageName;
+        if (url === '/') {
+          pageName = 'index';
+        }
+        else {
+          pageName = path.posix.join(path.dirname(url), path.basename(url, '.html')).slice(1);
+        }
+
+        const page = options?.pages?.[pageName];
+
+        if (!page) {
+          return next();
+        }
+
+        const templateOption = page.template;
+        const templatePath = templateOption ? resolve(templateOption) : resolve('public/index.html');
+
+        const content = await getHtmlContent({
+          templatePath,
+          pageEntry: page.entry || 'main',
+          pageTitle: page.title || 'Home Page',
+          data: options.data
+        });
+
+        res.end(content)
+      })
+    },
+
+    resolveId(id) {
+      if (path.extname(id) === '.html') {
+        const relativeId = relative(id);
+        const pageName = path.posix.join(path.dirname(relativeId), path.basename(relativeId, '.html'));
+
+        const page = options?.pages?.[pageName];
+        if (page) {
+          return id;
+        }
+      }
+
+      return null;
+    },
+
+    load(id) {
+      if (path.extname(id) === '.html') {
+        const relativeId = relative(id);
+        const pageName = path.posix.join(path.dirname(relativeId), path.basename(relativeId, '.html'));
+
+        const page = options?.pages?.[pageName];
+        if (page) {
+          const templateOption = page.template;
+          const templatePath = templateOption ? resolve(templateOption) : resolve('public/index.html');
+
+          return getHtmlContent({
+            templatePath,
+            pageEntry: page.entry || 'main',
+            pageTitle: page.title || 'Home Page',
+            data: options.data
+          });
+        }
+      }
+
+      return null;
+    }
+  }
+}
+
+export default virtualHtmlTemplatePlugin;
